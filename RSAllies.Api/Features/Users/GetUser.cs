f61ae78a8faa8ae3516a1bb518e813061b98f0b4
hei karata <herishine00@gmail.com>
2024-04-05 00:39:51 +0300

    Added UserDTO, GetUser class, and GetUserEndPoint
    Added a new `UserDTO` record in the `RSAllies.Api.Contracts` namespace with read-only properties. Implemented a `GetUser` class in the `RSAllies.Api.Features.Users` namespace with a `Query` class and an internal `Handler` class for fetching user data from the database. Also added a `GetUserEndPoint` class that maps a GET request to the `/api/users/{id}` endpoint, returning user data or a 404 status.

---
+++ RSAllies.Api/Features/Users/GetUser.cs
@@ -0,0 +1,52 @@
+﻿using Carter;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using RSAllies.Api.Contracts;
+using RSAllies.Api.Data;
+using RSAllies.Api.HelperTypes;
+
+namespace RSAllies.Api.Features.Users
+{
+    public static class GetUser
+    {
+
+        public class Query : IRequest<Result<UserDTO>>
+        {
+            public Guid Id { get; set; }
+        }
+
+        internal class Handler(AppDbContext _context) : IRequestHandler<Query, Result<UserDTO>>
+        {
+            public async Task<Result<UserDTO>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var user = await _context.Users
+                    .AsNoTracking()
+                    .Where(u => u.Id == request.Id)
+                    .Select(u => new UserDTO { Id = u.Id, FirstName = u.FirstName, LastName = u.LastName, Email = u.Email, Phone = u.Phone})
+                    .SingleOrDefaultAsync(cancellationToken);
+
+                if (user == null)
+                    return Result.Failure<UserDTO>(new Error("GetUser.NonexistentUser", "The specified user does not exist"));
+
+                return user;
+            }
+        }
+    }
+
+    public class GetUserEndPoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/api/users/{id}", async (Guid Id, ISender sender) =>
+            {
+                var result = await sender.Send(new GetUser.Query { Id = Id });
+
+                if (result.IsFailure)
+                    return Results.NotFound(result.Error);
+
+                return Results.Ok(result.Value);
+
+            });
+        }
+    }
+}
