c1fb1c79a03e4eb6ad933ca22d8294e619eb3fc4
hei karata <herishine00@gmail.com>
2024-04-06 14:40:08 +0300

    Added project references and updated builder
    Added project references in `RSAllies.Api.csproj`, `RSAllies.AppHost.csproj`, and `RSAllies.csproj` to `RSAllies.ServiceDefaults.csproj` and `RSAllies\RSAllies\RSAllies.csproj`. This establishes dependencies between these projects. Also, `Program.cs` was updated to include `Projects.RSAllies_Api` and `Projects.RSAllies` in the distributed application being built.

--- RSAllies.AppHost/Program.cs
+++ RSAllies.AppHost/Program.cs
@@ -1,3 +1,8 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var apiservice = builder.AddProject<Projects.RSAllies_Api>("apiservice");
+
+builder.AddProject<Projects.RSAllies>("web-admin")
+    .WithReference(apiservice);
+
 builder.Build().Run();
