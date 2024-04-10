d72f20ccfe94ccc2c40b2cffe905e46f5164a8a9
hei karata <herishine00@gmail.com>
2024-04-11 01:23:05 +0300

    Added Helper Type classes

---
+++ RSAllies/RSAllies/RSAllies.Client/HelperTypes/ResultT.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace RSAllies.Client.HelperTypes
+{
+    public class Result<TValue> : Result
+    {
+        private readonly TValue? _value;
+
+        [JsonConstructor]
+        protected internal Result(TValue? value, bool isSuccess, Error error)
+            : base(isSuccess, error) =>
+            _value = value;
+
+        public List? Value => IsSuccess
+            ? _value!
+            : throw new InvalidOperationException("The value of a failure result can not be accessed.");
+
+        public static implicit operator Result<TValue>(TValue? value) => Create(value);
+    }
+}
