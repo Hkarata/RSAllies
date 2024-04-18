e84d228105a266ae5f6dd78ffebae26477302756
hei karata <herishine00@gmail.com>
2024-04-18 20:45:52 +0300

    `Added Swahili question creation and retrieval functionality`
    ` `
    
    `Added new files for creating and retrieving Swahili questions. The `CreateSwahiliQuestion.cs` file includes classes for creating a new Swahili question and defining the endpoint. An empty `GetQuestions.cs` file has been added for future use. The `GetSwahiliQuestions.cs` file includes classes for retrieving a list of questions and defining the endpoint. Also, added migration files `20240418170955_QuestionLanguageMarker.Designer.cs` and `20240418170955_QuestionLanguageMarker.cs` for the `Question` entity, introducing a new `IsEnglish` column to the `Questions` table.`

---
+++ RSAllies.Api/Features/Questions/CreateSwahiliQuestion.cs
@@ -0,0 +1,93 @@
+﻿using Carter;
+using Mapster;
+using MediatR;
+using RSAllies.Api.Contracts;
+using RSAllies.Api.Data;
+using RSAllies.Api.Entities;
+using RSAllies.Api.HelperTypes;
+
+namespace RSAllies.Api.Features.Questions;
+
+public abstract class CreateSwahiliQuestion
+{
+ 
+    public class Command : IRequest<Result<bool>>
+    {
+        public Guid Id { get; set; }
+        
+        public string QuestionText { get; set; } = string.Empty;
+        
+        public List<CommandChoices>? ChoicesList { get; set; }
+        
+    }
+    
+    public class CommandChoices
+    {
+        public Guid Id { get; set; }
+        public string ChoiceText { get; set; } = string.Empty;
+        public bool IsAnswer { get; set; }
+    }
+    
+    internal sealed class Handler(AppDbContext context) : IRequestHandler<Command, Result<bool>>
+    {
+        public async Task<Result<bool>> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var question = new Question
+            {
+                Id = Guid.NewGuid(),
+                QuestionText = request.QuestionText,
+                Choices = request.ChoicesList!.Select(c => new Choice
+                {
+                    Id = Guid.NewGuid(),
+                    ChoiceText = c.ChoiceText,
+                    IsCorrect = c.IsAnswer
+                }).ToList(),
+                IsEnglish = false
+            };
+            
+            var correctChoice = question.Choices.FirstOrDefault(c => c.IsCorrect);
+            if (correctChoice != null)
+            {
+                var answer = new Answer
+                {
+                    Id = Guid.NewGuid(),
+                    QuestionId = question.Id,
+                    ChoiceId = correctChoice.Id
+                };
+
+                context.Add(answer);
+            }
+
+            context.Questions.Add(question);
+            await context.SaveChangesAsync(cancellationToken);
+
+            return true;
+        }
+    }
+    
+}
+
+public class CreateQuestionSwahiliEndPoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/api/question/swahili", async (QuestionDto question, ISender sender) =>
+        {
+            var request = new CreateSwahiliQuestion.Command
+            {
+                Id = question.Id,
+                QuestionText = question.QuestionText,
+                ChoicesList = question.Choices?.Select(c =>new CreateSwahiliQuestion.CommandChoices
+                {
+                    Id = c.Id,
+                    ChoiceText = c.ChoiceText,
+                    IsAnswer = c.IsAnswer
+                }).ToList()
+            };
+            
+            var result = await sender.Send(request);
+            
+            return result.IsFailure ? Results.BadRequest(result.Error) : Results.Ok(result);
+        });
+    }
+}
---
+++ RSAllies.Api/Migrations/20240418170955_QuestionLanguageMarker.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RSAllies.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class QuestionLanguageMarker : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEnglish",
+                table: "Questions",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsEnglish",
+                table: "Questions");
+        }
+    }
+}
