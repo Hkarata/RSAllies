96b799ab444974c68bf82e2a28063e1487c39421
hei karata <herishine00@gmail.com>
2024-04-21 16:23:11 +0300

    Enabled CORS and added new properties to models
    In this commit, CORS has been enabled in the application by uncommenting the relevant code in `Program.cs`. Two new properties, `CreatedAt` and `ScoreValue`, have been added to the `b` object in `AppDbContextModelSnapshot.cs`. A new migration file `20240421132146_AuditMigration.cs` and its corresponding design file `20240421132146_AuditMigration.Designer.cs` have been added. These files introduce a new `CreatedAt` column to the `Scores` table and update the database schema accordingly. The `CreatedAt` property has also been added to various entities in the `BuildTargetModel` method of the `AuditMigration` class.

--- RSAllies.Api/Migrations/20240421132146_AuditMigration.Designer.cs
+++ RSAllies.Api/Migrations/20240421132146_AuditMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RSAllies.Api.Data;
 
@@ -11,9 +12,11 @@ using RSAllies.Api.Data;
 namespace RSAllies.Api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240421132146_AuditMigration")]
+    partial class AuditMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- RSAllies.Api/Program.cs
+++ RSAllies.Api/Program.cs
@@ -29,15 +29,15 @@ builder.Services.AddMediatR(config =>
 
 builder.Services.AddCarter();
 
-//builder.Services.AddCors(options =>
-//{
-//	options.AddDefaultPolicy(builder =>
-//	{
-//		builder.AllowAnyOrigin()
-//			.AllowAnyMethod()
-//			.AllowAnyHeader();
-//	});
-//});
+builder.Services.AddCors(options =>
+{
+	options.AddDefaultPolicy(builder =>
+	{
+		builder.AllowAnyOrigin()
+			.AllowAnyMethod()
+			.AllowAnyHeader();
+	});
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -45,7 +45,7 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-//app.UseCors();
+app.UseCors();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
